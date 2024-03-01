0622db54ebbc803f4f678e9effa3f84ce1c30a45
Krzysztof Owsiany <gocom@o2.pl>
2024-03-01 23:51:53 +0100

    feat: init MAUI project

---
+++ src/FolderInboxZero/FolderInboxZero/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FolderInboxZero
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/FolderInboxZero/FolderInboxZero/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FolderInboxZero
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
